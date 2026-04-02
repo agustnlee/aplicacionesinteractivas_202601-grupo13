5fdfa7d3962dce73d7f5268496ae36224391476a
agustnlee <lyoonsoo25@gmail.com>
2026-04-02 19:36:56 -0300

    feat: CreditoRepository + CuotaRepository
    Adicionalmente: revision y mejora de entidades Cuota y Credito

--- backend/src/main/java/com/uade/tp13/repository/CreditoRepository.java
+++ backend/src/main/java/com/uade/tp13/repository/CreditoRepository.java
@@ -3,6 +3,7 @@ package com.uade.tp13.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.uade.tp13.enums.EstadoCredito;
 import com.uade.tp13.model.Credito;
 
 import java.util.List;
@@ -10,5 +11,11 @@ import java.util.List;
 @Repository
 public interface CreditoRepository extends JpaRepository<Credito, Long> {
 
-    List<Credito> findByClienteDni(String dni);
+    List<Credito> findByEstado(EstadoCredito estado);
+    List<Credito> findByCliente_Id(Long clienteId);
+    List<Credito> findByCobrador_Id(Long cobradorId);
+    List<Credito> findByCobrador_IdAndEstado(Long cobradorId, EstadoCredito estado);
+    List<Credito> findByCreadoPorId(Long creadoPorId);
+    
+    boolean existsByCliente_IdAndEstadoIn(Long clienteId, List<EstadoCredito> estados); // Busqueda con id cliente y estado, para validar soft delete de cliente (con activos)
 }
--- backend/src/main/java/com/uade/tp13/repository/CuotaRepository.java
+++ backend/src/main/java/com/uade/tp13/repository/CuotaRepository.java
@@ -4,12 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.uade.tp13.model.Cuota;
-import com.uade.tp13.model.CuotaId;
 
 import java.util.List;
 
 @Repository
-public interface CuotaRepository extends JpaRepository<Cuota, CuotaId> {
+public interface CuotaRepository extends JpaRepository<Cuota, Long> {
 
-    List<Cuota> findByIdIdCredito(Long idCredito);
-}
+    List<Cuota> findByCredito_Id(Long creditoId);
+    List<Cuota> findByCredito_IdAndPagada(Long creditoId, Boolean pagada);
+    List<Cuota> findByPagadaFalse();
+
+    boolean existsByCredito_IdAndPagada(Long creditoId, Boolean pagada); // para chequear si quedan impagas
+}   
+
