43f593c4997d8fd546a1d77fb63205f92bd4543c
agustnlee <lyoonsoo25@gmail.com>
2026-04-02 23:44:11 -0300

    feat: CreditoService + CuotaService
    TODOs para conectar con otras entidades + se agregaron helpers publicos

--- backend/src/main/java/com/uade/tp13/service/CuotaService.java
+++ backend/src/main/java/com/uade/tp13/service/CuotaService.java
@@ -1,5 +1,60 @@
 package com.uade.tp13.service;
 
+import com.uade.tp13.dto.response.CuotaResponse;
+import com.uade.tp13.enums.EstadoCuota;
+import com.uade.tp13.repository.CuotaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CuotaService {
+
+    private final CuotaRepository cuotaRepository;
+    private final CreditoService creditoService;
+
+    // Consultas
+
+    // Ver TODOS
+    @Transactional(readOnly = true)
+    public List<CuotaResponse> obtenerTodas(Long creditoId) {
+        creditoService.getOrThrow(creditoId);
+        return cuotaRepository.findByCredito_Id(creditoId)
+                .stream()
+                .map(creditoService::mapCuotaToResponse)
+                .toList();
+    }
+
+    // Ver PENDIENTE y fecha FUTURA
+    @Transactional(readOnly = true)
+    public List<CuotaResponse> obtenerPendientes(Long creditoId) {
+        creditoService.getOrThrow(creditoId);
+        return cuotaRepository
+                .findByCredito_IdAndPagada(creditoId, EstadoCuota.PENDIENTE)
+                .stream()
+                .filter(c -> !LocalDate.now().isAfter(c.getFechaVencimiento()))
+                .map(creditoService::mapCuotaToResponse)
+                .toList();
+    }
+
+    
+    // Ver Vencidas (estado derivado)
+    @Transactional(readOnly = true)
+    public List<CuotaResponse> obtenerVencidas(Long creditoId) {
+        creditoService.getOrThrow(creditoId);
+        return cuotaRepository
+                .findByCredito_IdAndPagada(creditoId, EstadoCuota.PENDIENTE)
+                .stream()
+                .filter(c -> LocalDate.now().isAfter(c.getFechaVencimiento()))
+                .map(creditoService::mapCuotaToResponse)
+                .toList();
+    }
+
+    // TODO: Registrar Pago
     
 }
