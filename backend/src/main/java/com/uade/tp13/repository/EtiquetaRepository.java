0ba72bcb6a353a5b7cba2adad96e40ea7ea6eccb
sofiaagustinamoral-dot <sofia.agustina.moral@gmail.com>
2026-04-04 13:59:09 -0300

    ultimo

--- backend/src/main/java/com/uade/tp13/repository/EtiquetaRepository.java
+++ backend/src/main/java/com/uade/tp13/repository/EtiquetaRepository.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.uade.tp13.model.Cliente;
 import com.uade.tp13.model.Etiqueta;
 
-import java.time.LocalDate;
-import java.util.List;
 
 @Repository
 public interface EtiquetaRepository extends JpaRepository<Etiqueta, long > {
