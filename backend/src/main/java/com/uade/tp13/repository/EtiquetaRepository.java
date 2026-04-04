8329bebd78fb816afb4fc09e7d890aa67eddfb1e
sofiaagustinamoral-dot <sofia.agustina.moral@gmail.com>
2026-04-03 21:39:42 -0300

    Correcciones

--- backend/src/main/java/com/uade/tp13/repository/EtiquetaRepository.java
+++ backend/src/main/java/com/uade/tp13/repository/EtiquetaRepository.java
@@ -8,15 +8,23 @@ import org.springframework.data.domain.Pageable;
 
 import com.uade.tp13.model.Etiqueta;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface EtiquetaRepository extends JpaRepository<Etiqueta, String > {
 
    
-    Page<Etiqueta> findByNombre(String Nombre, Pageable pageable);
+    Page<Etiqueta> findByNombre(String nombre, Pageable pageable);
+    Page<Etiqueta> findByDescripocion(String descripcion, Pageable pageable);
+    Page<Etiqueta> findByColor(String color, Pageable pageable);
 
-    List<Etiqueta> findByNombre(String Nombre, Pageable pageable);
+    
+
+    List<Etiqueta> findByNombre(String nombre);
+   
+   
+
 
     boolean existsByNombre(String Nombre);
 
