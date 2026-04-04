0ef7e8843b48441aa59a2a84adbe4f74e2df9148
Ale1406 <ale.orellana838@gmail.com>
2026-04-04 18:29:15 -0300

    feat:Implementacion DTO Cliente

--- backend/src/main/java/com/uade/tp13/dto/request/ClienteUpdateRequest.java
+++ backend/src/main/java/com/uade/tp13/dto/request/ClienteUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.uade.tp13.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
@@ -8,18 +7,10 @@ import lombok.Data;
 
 @Data
 @Builder
-public class ClienteRequest {
-
-    @NotBlank(message = "El DNI es obligatorio")
-    @Pattern(regexp = "//d{7,8}",message = "El DNI debe tener entre 7 u 8 digitos")
-    private String dni;
+public class ClienteUpdateRequest {
 
     @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
-
-    @NotBlank(message = "El email es obligatorio")
-    @Email(message = "El email tiene que ser con el formato valido")
-    private String email;
 
     @NotBlank(message = "El telefono es obligatorio")
     @Pattern(regexp = "//d{8,9}",message = "El telefono debe tener 8 numeros")
@@ -27,5 +18,9 @@ public class ClienteRequest {
 
     @NotBlank(message = "El domicilio es obligatorio")
     private String domicilio;
+   
+    @NotBlank(message = "El estado es obligatorio")
+    private Boolean estado;
+
 
 }
--- backend/src/main/java/com/uade/tp13/dto/response/ClienteResponse.java
+++ backend/src/main/java/com/uade/tp13/dto/response/ClienteResponse.java
@@ -1,12 +1,21 @@
 package com.uade.tp13.dto.response;
 
-import lombok.AllArgsConstructor;
+
+import lombok.Builder;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
+@Builder
 public class ClienteResponse {
 
     private String dni;
     private String nombre;
+    private String email;
+    private String telefono;
+    private String domicilio;
+    private String estado;
+
+    private Long creadoPor;
+    private String nombreCreadoPor;
+
 }
