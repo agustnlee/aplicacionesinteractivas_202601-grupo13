f1fb393eb9a10f4ff02ec451048779b31bff555d
Ale1406 <ale.orellana838@gmail.com>
2026-04-04 01:51:53 -0300

    feat:Implementacion Repositorio Cliente y Usuario
    Para la busqueda de usuario y clientes se solicita unos filtros en las HU que especifique en cada repo, por eso la solucion que vimos viable es hacer la consulta directa donde se seleccciona lo que quiere buscar.

--- backend/src/main/java/com/uade/tp13/repository/UsuarioRepository.java
+++ backend/src/main/java/com/uade/tp13/repository/UsuarioRepository.java
@@ -1,16 +1,29 @@
 package com.uade.tp13.repository;
-
+ 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.uade.tp13.enums.ROL_USUARIO;
 import com.uade.tp13.model.Usuario;
-
-import java.util.Optional;
-
+ 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-    Optional<Usuario> findByUsername(String username);
-
-    boolean existsByUsername(String username);
+ 
+    boolean existsByEmail(String email);
+    //REVISAR HU22
+    @Query("SELECT u FROM Usuario u WHERE " +
+           "(:nombre IS NULL OR LOWER(u.nombre) LIKE LOWER(CONCAT('%', :nombre, '%'))) AND " +
+           "(:rol IS NULL OR u.rol = :rol) AND " +
+           "(:estado IS NULL OR u.estado = :estado)")
+    Page<Usuario> buscarUsuariosConFiltro(
+            @Param("nombre") String nombre,
+            @Param("rol") ROL_USUARIO rol,
+            @Param("estado") Boolean estado,
+            Pageable pageable
+    );
 }
+ 
