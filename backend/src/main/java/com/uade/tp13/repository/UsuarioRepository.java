23555e3fcb56d73d6da3f3555f5c07b48b619b27
Ale1406 <ale.orellana838@gmail.com>
2026-04-04 02:08:36 -0300

    Falto un metodo para complementar Autenticacion

--- backend/src/main/java/com/uade/tp13/repository/UsuarioRepository.java
+++ backend/src/main/java/com/uade/tp13/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.uade.tp13.repository;
- 
+
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +14,8 @@ import com.uade.tp13.model.Usuario;
  
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
- 
+
+    Optional<Usuario> findByEmail( String email);
     boolean existsByEmail(String email);
     //REVISAR HU22
     @Query("SELECT u FROM Usuario u WHERE " +
