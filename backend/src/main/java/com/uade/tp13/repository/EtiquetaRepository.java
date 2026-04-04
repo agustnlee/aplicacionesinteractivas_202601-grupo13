5447f5c31e8dd5b6912d180666ffab5e3bbd3479
sofiaagustinamoral-dot <sofia.agustina.moral@gmail.com>
2026-04-04 15:56:03 -0300

    otros cambios

--- backend/src/main/java/com/uade/tp13/repository/EtiquetaRepository.java
+++ backend/src/main/java/com/uade/tp13/repository/EtiquetaRepository.java
@@ -3,12 +3,12 @@ package com.uade.tp13.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.uade.tp13.model.Cliente;
+
 import com.uade.tp13.model.Etiqueta;
 
 
 @Repository
-public interface EtiquetaRepository extends JpaRepository<Etiqueta, long > {
+public interface EtiquetaRepository extends JpaRepository<Etiqueta, Long > {
 
     Optional Binary <Etiqueta> findByNombre(String Nombre);
     
