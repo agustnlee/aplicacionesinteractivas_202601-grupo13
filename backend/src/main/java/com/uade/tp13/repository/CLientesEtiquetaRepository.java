612f256b7ba6c846c1234b6a50925af526cf03ff
sofiaagustinamoral-dot <sofia.agustina.moral@gmail.com>
2026-04-04 15:52:37 -0300

    Cambios

---
+++ backend/src/main/java/com/uade/tp13/repository/CLientesEtiquetaRepository.java
@@ -0,0 +1,46 @@
+package com.uade.tp13.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.uade.tp13.model.ClientesEtiqueta;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface ClientesEtiquetaRepository extends JpaRepository<ClientesEtiqueta, Long> {
+
+    Page<ClientesEtiqueta> findByEtiquetaId (long etiqueta_id, Pageable pageable);
+   //HU47
+
+    Page<ClientesEtiqueta> findByClienteId(long cliente_id, Pageable pageable);
+    //Hu45
+
+
+    Page<ClientesEtiqueta> findByAsignadoPorId(long asignado_por_id, Pageable pageable);
+    //Hu48
+ 
+
+    
+
+    
+
+    boolean existsById(long id);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
