5aa4052c053618cefbc4e26c79ec3c4081995ab1
Nicolás Benitez <nicolasagustinb1@gmail.com>
2026-04-09 17:10:04 -0300

    moví el cód a la carpeta tp

--- backend/src/main/java/com/uade/tp13/repository/PagoRepository.java
+++ backend/src/main/java/com/uade/tp13/repository/PagoRepository.java
@@ -1,10 +1,4 @@
-package com.uade.tp13.repository;
-
-import com.uade.tp13.model.Pago;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 @Repository
 public interface PagoRepository extends JpaRepository<Pago, Long> {
-    // Hereda save, findById, etc.
+    List<Pago> findByCuota_Credito_Id(Long creditoId); // Para el GET pagos de un crédito
 }
--- backend/src/main/java/com/uade/tpejemplo/repository/PagoRepository.java
+++
@@ -1,4 +0,0 @@
-@Repository
-public interface PagoRepository extends JpaRepository<Pago, Long> {
-    List<Pago> findByCuota_Credito_Id(Long creditoId); // Para el GET pagos de un crédito
-}
