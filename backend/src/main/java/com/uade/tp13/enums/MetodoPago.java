ddc0072b2ad9457bbb874cc49045c17c35657921
agustnlee <lyoonsoo25@gmail.com>
2026-04-08 23:35:40 -0300

    Chore: Movido archivos de pago de directorio tpejemplo a tp13

---
+++ backend/src/main/java/com/uade/tp13/enums/MetodoPago.java
@@ -0,0 +1,8 @@
+package com.uade.tp13.enums;
+
+public enum MetodoPago {
+    EFECTIVO,
+    TARJETA,
+    TRANSFERENCIA,
+    OTRO
+}
--- backend/src/main/java/com/uade/tp13/model/Pago.java
+++ backend/src/main/java/com/uade/tp13/model/Pago.java
@@ -1,9 +1,6 @@
-package com.uade.tpejemplo.model;
+package com.uade.tp13.model;
 
 import com.uade.tp13.enums.MetodoPago;
-import com.uade.tp13.model.Cuota;
-import com.uade.tp13.model.Usuario;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
