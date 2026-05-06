f08307aa6355beb5cec8ed7464a232ace5aea1c7
agustnlee <lyoonsoo25@gmail.com>
2026-05-05 22:21:51 -0300

    chore: added new icon types for react lucide

--- frontend/src/utils/icontypes.js
+++ frontend/src/utils/icontypes.js
@@ -1,7 +1,16 @@
-import { Trash2, Plus, PencilLine } from "lucide-react";
+import { Trash2, Plus, PencilLine, ArrowDownToLine, LogOut, SquarePen, CircleUser, Info, Check, CircleAlert } from "lucide-react;
+import { FaGithub } from 'react-icons/fa';
 
 export const ICONS = {
   trash: Trash2,
   plus: Plus,
   edit: PencilLine,
+  arrowDownToLine: ArrowDownToLine,
+  logout: LogOut,
+  pencil: SquarePen,
+  user: CircleUser,
+  info: Info,
+  check: Check,
+  alert: CircleAlert,
+  
 };
