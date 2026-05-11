170a07f68b16b5a1e2a429938923373a6a967405
agustnlee <lyoonsoo25@gmail.com>
2026-05-11 14:10:34 -0300

    chore: Delete Row.jsx + Padding for .btn

--- frontend/src/components/common/Row.jsx
+++
@@ -1,9 +0,0 @@
-// reusable component for maybe etiquetas listado general
-
-export default function Row({ children }) {
-  return (
-    <div style={{ display: "flex", gap: "12px", flexWrap: "wrap" }}>
-      {children}
-    </div>
-  );
-}
