33346d4d6c74e3835b62ed1ea1c4fd1b3fce7d30
sofiaagustinamoral-dot <sofia.agustina.moral@gmail.com>
2026-05-18 13:40:37 -0300

    primer commit

--- frontend/src/pages/etiquetas/EtiquetasDetail.jsx
+++ frontend/src/pages/etiquetas/EtiquetasDetail.jsx
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
 
+
+
 export default function EtiquetasDetail() {
     const { id } = useParams();
+    const mockEtiqueta ={id:1, nombre: "mora", color: "green", descripcion: "blabla"}
 
     return (<> 
+
+    <div>
+
+        <h1>Etiqueta: </h1>
+        
+        
+
+
+    </div>
         <p> funciona /EtiquetasDetail {id}</p>
         
         
