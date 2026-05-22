aab35ad8a4e4df973f029864c5f470bd68aa35d5
agustnlee <lyoonsoo25@gmail.com>
2026-05-22 02:06:40 -0300

    chore: changed user icon for without circle for iconsize bigger

--- frontend/src/utils/icontypes.js
+++ frontend/src/utils/icontypes.js
@@ -1,4 +1,4 @@
-import { Trash2, Plus, PencilLine, ArrowDownToLine, LogOut, SquarePen, CircleUser, Info, Check, CircleAlert, GitBranch, Menu, LogIn, CircleChevronRight, ShieldCheck, CirclePlus, BookmarkPlus, BookmarkMinus, Bookmark, TagsIcon, AtSign, CircleCheck, CircleCheckBig, ClipboardPen, Palette, Hash, LockOpen, Lock, PencilLineIcon, Mail, Eye, EyeOff } from "lucide-react";
+import { Trash2, Plus, PencilLine, ArrowDownToLine, LogOut, SquarePen, Info, Check, CircleAlert, GitBranch, Menu, LogIn, CircleChevronRight, ShieldCheck, CirclePlus, BookmarkPlus, BookmarkMinus, Bookmark, TagsIcon, AtSign, CircleCheck, CircleCheckBig, ClipboardPen, Palette, Hash, LockOpen, Lock, PencilLineIcon, Mail, Eye, EyeOf, User } from "lucide-react";
 import { FaGithub } from 'react-icons/fa';
 
 export const ICONS = {
@@ -9,7 +9,7 @@ export const ICONS = {
   logout: LogOut,
   pencil: SquarePen,
   pencilLine: PencilLineIcon,
-  user: CircleUser,
+  user: User,
   info: Info,
   check: Check,
   alert: CircleAlert,
