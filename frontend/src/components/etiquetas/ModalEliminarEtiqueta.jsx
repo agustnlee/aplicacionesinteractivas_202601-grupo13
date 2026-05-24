d6c4c845a4281fd03e9924b51d9e287f92c8ba20
sofiaagustinamoral-dot <sofia.agustina.moral@gmail.com>
2026-05-24 16:05:27 -0300

    feature:eliminarEtiqueta

--- frontend/src/components/etiquetas/ModalEliminarEtiqueta.jsx
+++ frontend/src/components/etiquetas/ModalEliminarEtiqueta.jsx
@@ -1,53 +1,29 @@
-import { useState } from "react";
 import Modal from "../common/Modal";
 import { ICONS } from "../../utils/icontypes";
-import styles from "../creditos/ModalCambiarCobrador.module.css";
 
 
-export default function ModalEliminarEtiqueta({isOpen, onClose, onConfirm })
-{ 
-  const [error, setError]= useState("");
+export default function ModalEliminar({ isOpen, etiqueta, onClose, onConfirm }) {
+    if (!etiqueta) return null;
+   
 
- const handleConfirm = () => {
-    setError("");
-    onConfirm();
-  }
+   
 
-  
-  const handleClose = () => { 
-    setNombre(""); 
-    setError(""); 
-    onClose();
-  }
-
-   return (
-          <Modal
-              isOpen={isOpen}
-              onClose={handleClose}
-              title="Modificar nombre"
-              description="Ingresá el nombre nuevo de esta etiqueta."
-              icon={ICONS.edit}
-              iconVariant="default"
-              size="sm"
-              actions={[{ label: "Confirmar", onClick: handleConfirm, variant: "primary" }]}
-          >
-              <div className={styles.body}>
-                  <label className={styles.label}> NOMBRE ETIQUETA</label>
-                  <input
-                      type="text"
-                      value={nombre}
-                      onChange={e => { setNombre(e.target.value); setError(""); }}
-                      placeholder="Ej: aaa"
-                      className={`${styles.input} ${error ? styles.inputError : ""}`}
-                  />
-                  {error && <span className={styles.error}>{error}</span>}
-              </div>
-          </Modal>
-      );
-  }
-
-
-
-
-
-
+    return (
+        <Modal
+            isOpen={isOpen}
+            onClose={onClose}
+            title={ "eliminar etiqueta" }
+            description={`¿Confirmás la eliminacion de ${etiqueta.nombre}? no podras deshacer esta accion`
+                    
+            }
+            icon={ICONS.trash }
+            iconVariant={ "danger" }
+            size="sm"
+            actions={[{
+                label:   "Eliminar" ,
+                onClick: onConfirm,
+                variant: "danger" ,
+            }]}
+        />
+    );
+}
