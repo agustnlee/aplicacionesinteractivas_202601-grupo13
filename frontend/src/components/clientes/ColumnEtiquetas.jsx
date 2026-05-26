bdcce4463e6bae0db20186226e1eeb1b2a63b0b7
agustnlee <lyoonsoo25@gmail.com>
2026-05-25 22:40:44 -0300

    fix: Nombre de import incorrecto

--- frontend/src/components/clientes/ColumnEtiquetas.jsx
+++ frontend/src/components/clientes/ColumnEtiquetas.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { obtenerEtiquetaPorCliente} from "../../api/apiClienteEtiquetas"; 
-import { obtenerEtiquetaPorId } from "../../api/apiEtiqueta"; 
+import { obtenerEtiquetaPorId } from "../../api/apiEtiquetas"; 
 import { Link } from "react-router-dom";
 
 export default function CeldaEtiquetas({ clienteId }) {
