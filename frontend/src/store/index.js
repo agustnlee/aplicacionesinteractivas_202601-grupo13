088766878cbb038ee1d762117e87674d68100123
Ezequiel-Souza-Arraigada <esouza@uade.edu.ar>
2026-06-20 15:27:40 -0300

    feat: agregar slice redux para usuarios

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import usuarioReducer from "./usuarioSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        usuario: usuarioReducer,
         // TODO: El resto de reducers
     }
 });
